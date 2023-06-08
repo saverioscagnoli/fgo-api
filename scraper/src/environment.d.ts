e1628f02310cd869e7659589d91adcae159cbbc0
Saverio <svscagn@gmail.com>
2023-06-09 01:50:59 +0200

    add stats to scraping

--- scraper/src/environment.d.ts
+++ scraper/src/environment.d.ts
@@ -4,7 +4,22 @@ declare global {
       WRAPPER: string;
       SERVANT_NAME: string;
       JP_NAME: string;
+      STARS: string;
       AKAS: string;
+      ID: string;
+      COST: string;
+      ATK: string;
+      HP: string;
+      GRAIL_ATK: string;
+      GRAIL_HP: string;
+      GRAIL_ATK_120: string;
+      GRAIL_HP_120: string;
+      VOICE_ACTOR: string;
+      ILLUSTRATOR: string;
+      ATTRIBUTES: string;
+      GROWTH_CURVE: string;
+      STAR_ABSORPTION: string;
+      STAR_GENERATION: string;
     }
   }
 }
--- scraper/src/index.ts
+++ scraper/src/index.ts
@@ -3,6 +3,7 @@ import { Cheerio, CheerioAPI, AnyNode, load } from "cheerio";
 import "dotenv/config";
 
 type Wrapper = Cheerio<AnyNode>;
+type Tuple = [number, number];
 
 const scrape = async (url: string): Promise<void> => {
   const { data } = await axios.get(url);
@@ -12,7 +13,22 @@ const scrape = async (url: string): Promise<void> => {
 
   let name = $(process.env.SERVANT_NAME).text().trim();
   let jpName = findJpName($, WRAPPER);
-  let akas = findAkas($, WRAPPER);
+  let stars = findStars($);
+  let akas = findAkas(WRAPPER);
+  let id = findID($, WRAPPER);
+  let cost = findCost(WRAPPER);
+  let atk = findBaseStats($, WRAPPER);
+  let hp = findBaseStats($, WRAPPER, process.env.HP);
+  let lvl100Atk = findGrailStats($, WRAPPER, process.env.GRAIL_ATK);
+  let lvl100Hp = findGrailStats($, WRAPPER, process.env.GRAIL_HP);
+  let lvl120Atk = findGrailStats($, WRAPPER, process.env.GRAIL_ATK_120);
+  let lvl120Hp = findGrailStats($, WRAPPER, process.env.GRAIL_HP_120);
+  let va = findVoiceActor(WRAPPER);
+  let illustrator = findIllustrator(WRAPPER);
+  let attribute = findAttributes(WRAPPER);
+  let growthCurve = findGrowthCurve(WRAPPER);
+  let starAbsorption = findStarAbsorption($, WRAPPER);
+  let starGen = findStarGen($, WRAPPER);
 };
 
 const findJpName = ($: CheerioAPI, wr: Wrapper): string => {
@@ -24,12 +40,129 @@ const findJpName = ($: CheerioAPI, wr: Wrapper): string => {
     .trim();
 };
 
-const findAkas = ($: CheerioAPI, wr: Wrapper): string[] => {
+const findStars = ($: CheerioAPI): string[] => {
+  return $(process.env.STARS)
+    .text()
+    .split("")
+    .filter(s => s === "★")
+    .map(() => "*");
+};
+
+const findAkas = (wr: Wrapper): string[] | null => {
   let akaStr = wr.find(process.env.AKAS).parent().text();
   return akaStr
-    .slice(4)
-    .replace(/\([^()]*\)/g, "")
+    ? akaStr
+        .slice(4)
+        .replace(/\([^()]*\)/g, "")
+        .trim()
+        .split(",")
+        .map(aka => aka.trim())
+    : null;
+};
+
+const findID = ($: CheerioAPI, wr: Wrapper): number => {
+  return +wr
+    .find("b")
+    .filter((_, el) => $(el).text().trim() === process.env.ID)
+    .parent()
+    .text()
     .trim()
-    .split(",")
-    .map(aka => aka.trim());
+    .replace(/^\D+/g, "");
+};
+
+const findCost = (wr: Wrapper): number => {
+  return +wr
+    .find(process.env.COST)
+    .parent()
+    .parent()
+    .text()
+    .trim()
+    .replace(/^\D+/g, "");
+};
+
+const findBaseStats = ($: CheerioAPI, wr: Wrapper, stat?: string): Tuple => {
+  stat = stat ?? process.env.ATK;
+  return wr
+    .find("b")
+    .filter((_, el) => $(el).text().trim() === stat)
+    .parent()
+    .parent()
+    .text()
+    .trim()
+    .split("/")
+    .map(stat => +stat.replace(/^\D+/g, "").replace(",", "")) as Tuple;
+};
+
+const findGrailStats = ($: CheerioAPI, wr: Wrapper, stat?: string) => {
+  return +wr
+    .find("b")
+    .filter((_, el) => $(el).text().trim() === stat)
+    .parent()
+    .parent()
+    .text()
+    .split(" ")
+    .slice(-1)[0]
+    .replace(",", "");
+};
+
+const findVoiceActor = (wr: Wrapper): string => {
+  return wr
+    .find(process.env.VOICE_ACTOR)
+    .parent()
+    .parent()
+    .text()
+    .split(":")[1]
+    .trim();
+};
+
+const findIllustrator = (wr: Wrapper): string => {
+  return wr
+    .find(process.env.ILLUSTRATOR)
+    .parent()
+    .parent()
+    .text()
+    .split(":")[1]
+    .trim();
+};
+
+const findAttributes = (wr: Wrapper): string => {
+  return wr
+    .find(process.env.ATTRIBUTES)
+    .parent()
+    .parent()
+    .text()
+    .split(":")[1]
+    .trim();
+};
+
+const findGrowthCurve = (wr: Wrapper): string => {
+  return wr
+    .find(process.env.GROWTH_CURVE)
+    .parent()
+    .parent()
+    .text()
+    .split(":")[1]
+    .trim();
+};
+
+const findStarAbsorption = ($: CheerioAPI, wr: Wrapper): number => {
+  return +wr
+    .find("b")
+    .filter((_, el) => $(el).text().trim() === process.env.STAR_ABSORPTION)
+    .parent()
+    .parent()
+    .text()
+    .split(":")[1]
+    .trim();
+};
+
+const findStarGen = ($: CheerioAPI, wr: Wrapper): string => {
+  return wr
+    .find("b")
+    .filter((_, el) => $(el).text().trim() === process.env.STAR_GENERATION)
+    .parent()
+    .parent()
+    .text()
+    .split(":")[1]
+    .trim();
 };
